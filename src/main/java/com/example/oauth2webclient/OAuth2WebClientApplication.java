0d35220bb2a709a00c8c5a437a2623f3942f93a1
vitaly.gusev <vitaly.gusev@progforce.com>
2024-01-12 15:48:29 +0300

    init 0.1

---
+++ src/main/java/com/example/oauth2webclient/OAuth2WebClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2WebClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2WebClientApplication.class, args);
+    }
+
+}
